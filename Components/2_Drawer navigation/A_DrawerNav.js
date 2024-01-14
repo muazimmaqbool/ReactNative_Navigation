43e3c40d7a6bc05e7bfee881733e1d712f7d66b3
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-01-14 11:20:50 +0530

    Drawer Nav

---
+++ Components/2_Drawer navigation/A_DrawerNav.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const A_DrawerNav = () => {
+  return (
+    <View>
+      <Text>A_DrawerNav</Text>
+    </View>
+  )
+}
+
+export default A_DrawerNav
+
+const styles = StyleSheet.create({})
