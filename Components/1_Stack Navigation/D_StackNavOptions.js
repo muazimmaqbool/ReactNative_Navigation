664845d342d6a7749aa6a102d725777d7112a994
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-20 10:56:42 +0530

    styling in stack

--- Components/1_Stack Navigation/D_StackNavOptions.js
+++ Components/1_Stack Navigation/D_StackNavOptions.js
@@ -3,16 +3,16 @@ import React from 'react'
 
 /*
 lec: 76
-Lets explore the options that the stack navigation supports:
-like : screen title, styling headers, content styles
+->Lets explore the options that the stack navigation supports:
+  like : screen title, styling headers, content styles
 
-Note: by default header title position is ios is middle and in android is in left
+Note: by default header title position in ios is middle and in android it's left
 
 ->Title: by default the name prop on the screen is shown as screen title and to customize it specify
          the title option in APP.js
          example: <Stack.Screen name="Home" component={HomeScreen} options={{title:"Welcome Home"}}/> 
 
--> Styling Headers: like changing font size, background color etc:
+->Styling Headers: like changing font size, background color etc:
         in options add another key headerStyle which is also an object:
         Example:<Stack.Screen
           name="Home"
@@ -26,23 +26,23 @@ Note: by default header title position is ios is middle and in android is in lef
         />
         Also we have headerRight and headerLeft to add content/component on the left and right side of the header text
             <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: "Welcome Home",
-            headerStyle: { backgroundColor: "teal" },
-            headerTintColor: "white",
-            headerTitleStyle: { fontWeight: "bold" },
-            headerRight: () => (
-              <Pressable onPress={() => alert("Menu Button Pressed")}>
-                <Text style={{ color: "black", fontSize: 16 }}>Menu</Text>
-              </Pressable>
-            ),
-          }}
-        />
+              name="Home"
+              component={HomeScreen}
+              options={{
+                title: "Welcome Home",
+                headerStyle: { backgroundColor: "teal" },
+                headerTintColor: "white",
+                headerTitleStyle: { fontWeight: "bold" },
+                headerRight: () => (
+                  <Pressable onPress={() => alert("Menu Button Pressed")}>
+                    <Text style={{ color: "black", fontSize: 16 }}>Menu</Text>
+                  </Pressable>
+                ),
+              }}
+            />
         headerLeft works in same way
 
--> Styling Content of the screens: using contentStyle Option
+->Styling Content of the screens: using contentStyle Option
 example: <Stack.Screen
           name="Home"
           component={HomeScreen}
