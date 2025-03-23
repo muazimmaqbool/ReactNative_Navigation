efb8c84c9bc33988185235283da7fb6c6f44febc
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-23 10:26:06 +0530

    toggle and jumpTo

--- Components/Screens/2_For Drawer Nav/DashBoardScreen.js
+++ Components/Screens/2_For Drawer Nav/DashBoardScreen.js
@@ -1,32 +1,35 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 /*
 Opening/toggling the drawer using a button
 */
 
-const DashBoardScreen = ({navigation}) => {
+const DashBoardScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>DashBoard Screen</Text>
-      <Button title='Toggle drawer' onPress={()=>navigation.toggleDrawer()}/>
-      <Button title='Go To Settings' onPress={()=>navigation.jumpTo("Settings")}/>
+      <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
+      <Button
+        title="Go To Settings"
+        onPress={() => navigation.jumpTo("Settings")}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default DashBoardScreen
+export default DashBoardScreen;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center",
-    gap:10,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
   },
-  text:{
-    fontSize:25,
-    fontWeight:"bold",
-    marginBottom:16
-  }
-})
+  text: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+});
